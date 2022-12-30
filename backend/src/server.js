572b28dbf86a2fc8db6d6bd7b8bc1de69403842e
sajalkhan <sajalhossen@yahoo.com>
2022-12-30 12:09:23 +0600

    update changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,42 +1,22 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const config = require('config');
-
-const connectDB = require('./Config/db');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const port = process.env.PORT || 8080;
 
-// Connect Database
-connectDB();
-
-//Init req.body Middleware
-app.use(express.json({ extended: false }));
 app.use(cors());
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(express.json());
 
 // Define Routes
-app.use('/api/contact', require('./Routes/userContact'));
-const PORT = process.env.PORT || 8080;
+app.use("/api/contact", require("./Routes/userContact"));
 
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' }); // here will pass an object to env so it will understand where the configuration file is located
-const environment = process.env.NODE_ENV;
-
-if(process.env.NODE_ENV === environment)
-{
-    app.use(express.static('frontend/build'));
-    
-    app.get('*', (req, res)=> {
-        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
-    })
+// get driver connection
+const connectDB = require("./Config/db");
+try {
+  connectDB();
+  app.listen(port, () => console.log(`server started on port ${port}`));
+} catch (error) {
+  console.log("error mongodb connection");
 }
-
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
--- frontend/src/Action/ContactAction.js
+++ frontend/src/Action/ContactAction.js
@@ -1,192 +1,218 @@
-import axios from 'axios';
-import { ADD_NEW_CONTACT, DELETE_CONTACT, ERROR_MESSAGE, REMOVE_MESSAGE, GET_ALL_CONTACT } from './type';
+import axios from "axios";
 
+import {
+  ADD_NEW_CONTACT,
+  DELETE_CONTACT,
+  ERROR_MESSAGE,
+  REMOVE_MESSAGE,
+  GET_ALL_CONTACT,
+} from "./type";
 
+const baseUrl = () => {
+  return process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_BASE_URL_DEV
+    : process.env.REACT_APP_BASE_URL_PROD;
+};
+
+console.log("base url -", baseUrl);
 //Get All Contact
 export const GetAllContact = () => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-    try {
-        const res = await axios.get('/api/contact');
-        dispatch({
-            type: GET_ALL_CONTACT,
-            payload: res.data
-        })
-    } catch (err) {
-        console.log(err);
-    }
-}
+    const res = await axios.get(`${baseUrl()}/api/contact`, config);
+    dispatch({
+      type: GET_ALL_CONTACT,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log("error on get contact-", err);
+  }
+};
 
 //Add new Contact
 export const AddNewContact = (name, phone) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
+  const body = JSON.stringify({ name, phone });
+
+  try {
+    const res = await axios.post(
+      `${baseUrl()}/api/contact/addnewContact`,
+      body,
+      config
+    );
+    if (!res.data.error) {
+      dispatch({
+        type: ADD_NEW_CONTACT,
+        payload: res.data,
+      });
+
+      dispatch({
+        type: ERROR_MESSAGE,
+        payload: {
+          header: "Success!",
+          message: "Contact Added Successfully!",
+          error: true,
+          color: "green",
+        },
+      });
+
+      setTimeout(() => {
+        dispatch({
+          type: REMOVE_MESSAGE,
+          payload: {
+            message: "",
+            error: false,
+          },
+        });
+      }, 3000);
+    } else {
+      dispatch({
+        type: ERROR_MESSAGE,
+        payload: {
+          message: res.data.message,
+          header: "Error!",
+          error: true,
+          color: "red",
+        },
+      });
+
+      setTimeout(() => {
+        dispatch({
+          type: REMOVE_MESSAGE,
+          payload: {
+            message: "",
+            error: false,
+          },
+        });
+      }, 3000);
     }
-
-    const body = JSON.stringify({ name, phone });
-
-    try {
-        const res = await axios.post('/api/contact/addnewContact', body, config);
-        if (!res.data.error) {
-            dispatch({
-                type: ADD_NEW_CONTACT,
-                payload: res.data
-            });
-
-            dispatch({
-                type: ERROR_MESSAGE,
-                payload: {
-                    header: 'Success!',
-                    message: 'Contact Added Successfully!',
-                    error: true,
-                    color: 'green'
-                }
-            });
-
-            setTimeout(() => {
-                dispatch({
-                    type: REMOVE_MESSAGE,
-                    payload: {
-                        message: '',
-                        error: false
-                    }
-                })
-            }, 3000);
-        }
-        else {
-            dispatch({
-                type: ERROR_MESSAGE,
-                payload: {
-                    message: res.data.message,
-                    header: 'Error!',
-                    error: true,
-                    color: 'red'
-                }
-            });
-
-            setTimeout(() => {
-                dispatch({
-                    type: REMOVE_MESSAGE,
-                    payload: {
-                        message: '',
-                        error: false
-                    }
-                })
-            }, 3000);
-        }
-    } catch (err) {
-        console.log(err);
-    }
-}
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //Update Contact
 export const UpdateContact = (name, phone, id) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
+  const body = JSON.stringify({ name, phone, id });
+
+  try {
+    const res = await axios.put(
+      `${baseUrl()}/api/contact/editContact`,
+      body,
+      config
+    );
+
+    dispatch(GetAllContact());
+
+    if (
+      res.data.message === `${phone} number exists, choose another number` ||
+      res.data.message === "Contact Information is Already exists!"
+    ) {
+      dispatch({
+        type: ERROR_MESSAGE,
+        payload: {
+          message: res.data.message,
+          error: true,
+          color: "red",
+          header: "Error!",
+        },
+      });
+
+      setTimeout(() => {
+        dispatch({
+          type: REMOVE_MESSAGE,
+          payload: {
+            message: "",
+            error: false,
+          },
+        });
+      }, 3000);
+    } else {
+      dispatch({
+        type: ERROR_MESSAGE,
+        payload: {
+          message: res.data.message,
+          error: true,
+          color: "green",
+          header: "Edit!",
+        },
+      });
+
+      setTimeout(() => {
+        dispatch({
+          type: REMOVE_MESSAGE,
+          payload: {
+            message: "",
+            error: false,
+          },
+        });
+      }, 3000);
     }
-
-    const body = JSON.stringify({ name, phone, id });
-
-    try {
-        const res = await axios.put('/api/contact/editContact', body, config);
-
-        dispatch(GetAllContact());
-
-        if (res.data.message === `${phone} number exists, choose another number` || res.data.message ==='Contact Information is Already exists!') {
-            dispatch({
-                type: ERROR_MESSAGE,
-                payload: {
-                    message: res.data.message,
-                    error: true,
-                    color: 'red',
-                    header: 'Error!'
-                }
-            });
-
-            setTimeout(() => {
-                dispatch({
-                    type: REMOVE_MESSAGE,
-                    payload: {
-                        message: '',
-                        error: false,
-                    }
-                })
-            }, 3000);
-        }
-        else {
-            dispatch({
-                type: ERROR_MESSAGE,
-                payload: {
-                    message: res.data.message,
-                    error: true,
-                    color: 'green',
-                    header: 'Edit!'
-                }
-            });
-
-            setTimeout(() => {
-                dispatch({
-                    type: REMOVE_MESSAGE,
-                    payload: {
-                        message: '',
-                        error: false,
-                    }
-                })
-            }, 3000);
-        }
-
-    } catch (err) {
-        console.log(err);
-    }
-}
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 //Delete Contact
 export const DeleteContact = (id) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
+  const body = JSON.stringify({ id });
 
-    const body = JSON.stringify({ id });
+  try {
+    const res = await axios.post(
+      `${baseUrl()}/api/contact/DeleteContact`,
+      body,
+      config
+    );
 
-    try {
-        const res = await axios.post('/api/contact/DeleteContact', body, config);
-        
-        dispatch({
-            type: DELETE_CONTACT,
-            payload: id
-        });
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id,
+    });
 
-        console.log('res -- ', res);
+    console.log("res -- ", res);
 
-        dispatch({
-            type: ERROR_MESSAGE,
-            payload: {
-                message: 'Contact Deleted Successfully!',
-                error: true,
-                color: 'green',
-                header: 'Deleted!'
-            }
-        });
+    dispatch({
+      type: ERROR_MESSAGE,
+      payload: {
+        message: "Contact Deleted Successfully!",
+        error: true,
+        color: "green",
+        header: "Deleted!",
+      },
+    });
 
-        setTimeout(() => {
-            dispatch({
-                type: REMOVE_MESSAGE,
-                payload: {
-                    message: '',
-                    error: false
-                }
-            })
-        }, 3000);
-
-    } catch (err) {
-        console.log(err);
-    }
-}
+    setTimeout(() => {
+      dispatch({
+        type: REMOVE_MESSAGE,
+        payload: {
+          message: "",
+          error: false,
+        },
+      });
+    }, 3000);
+  } catch (err) {
+    console.log(err);
+  }
+};
